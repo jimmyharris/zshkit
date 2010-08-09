f2e5fe9cd35df739ede3a247605e62c35279d5b8
James Harris <jimmy@jamesharris.org>
2010-08-09 13:14:20 -0500

    Integates features of zsh-osx-tempaltes.
    I really like some behavior of the OS X templates but not all of them.
    I need to go through it line by line and reset some of the options to be
    more happy.

---
+++ 02_options.zsh
@@ -0,0 +1,88 @@
+setopt  	    append_history       
+setopt  	    auto_list            
+setopt  	    auto_menu            
+setopt  	    auto_param_keys      
+setopt  	    auto_pushd           
+setopt  	    bad_pattern          
+setopt  	    bang_hist            
+setopt  	    brace_ccl            
+setopt  	    correct_all          
+setopt  	    cdable_vars          
+setopt  	 NO_chase_links          
+setopt  	 NO_clobber              
+setopt  	    complete_in_word     
+setopt  	 NO_csh_junkie_loops     
+setopt  	 NO_csh_junkie_quotes    
+setopt  	 NO_csh_null_glob        
+setopt  	    extended_glob        
+setopt  	    function_argzero     
+setopt  	    glob                 
+setopt  	 NO_glob_assign          
+setopt  	    glob_complete        
+setopt  	 NO_glob_dots            
+setopt  	 NO_glob_subst           
+setopt  	    hash_cmds            
+setopt  	    hash_dirs            
+setopt  	    hash_list_all        
+setopt  	    hist_allow_clobber   
+setopt  	    hist_beep            
+setopt  	 NO_hup                  
+setopt  	 NO_ignore_braces        
+setopt  	    ignore_eof           
+setopt  	    interactive_comments 
+setopt  	 NO_list_ambiguous       
+setopt  	    list_types           
+setopt  	    long_list_jobs       
+setopt  	    magic_equal_subst    
+setopt  	 NO_mark_dirs            
+setopt  	    multios              
+setopt  	    nomatch              
+setopt  	    nohup                
+setopt  	    notify               
+setopt  	 NO_null_glob            
+setopt  	    path_dirs            
+setopt  	    posix_builtins       
+setopt  	 NO_print_exit_value     
+setopt  	    pushd_ignore_dups    
+setopt  	 NO_pushd_minus          
+setopt  	    pushd_to_home        
+setopt  	    rc_expand_param      
+setopt  	 NO_rc_quotes            
+setopt  	 NO_rm_star_silent       
+setopt  	 NO_sh_file_expansion    
+setopt  	    short_loops          
+setopt  	 NO_single_line_zle      
+setopt  	 NO_sun_keyboard_hack    
+setopt unset NO_verbose                 
+# setopt  	    zle       
+# setopt  	 NO_all_export           
+# setopt  	    always_last_prompt   
+# setopt  	 NO_always_to_end        
+# setopt  	 NO_auto_cd              
+# setopt  	 NO_auto_name_dirs       
+# setopt  	    auto_param_slash     
+# setopt  	    auto_remove_slash    
+# setopt  	 NO_auto_resume          
+# setopt  	 NO_beep                 
+# setopt  	 NO_bsd_echo             
+# setopt  	    complete_aliases     
+# setopt  	 NO_correct              
+# setopt  	    csh_junkie_history   
+# setopt  	    equals               
+# setopt  	    extended_history     
+# setopt  	    hist_ignore_dups     
+# setopt  	    hist_ignore_space    
+# setopt  	 NO_hist_no_store        
+# setopt  	    hist_verify          
+# setopt  	    share_history        
+# setopt  	 NO_ignore_eof           
+# setopt  	 NO_list_beep            
+# setopt  	 NO_mail_warning         
+# setopt  	 NO_menu_complete        
+# setopt  	    numeric_glob_sort    
+# setopt  	 NO_overstrike           
+# setopt  	 NO_prompt_cr            
+# setopt  	    prompt_subst         
+# setopt  	    pushd_silent         
+# setopt  	    sh_option_letters    
+# setopt  	    NO_sh_word_split     
