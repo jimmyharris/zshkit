92e4c6195935aafd4918ee164cab6f52969867e3
James Harris <jimmy@jamesharris.org>
2010-08-29 17:03:44 -0500

    Adds Runinstall alias.
    Now also sets R_LIBS_SITE so that I can install directly there. It might
    be better to set these automatically via functions, who knows.

--- 06_r_stat.zsh
+++ 06_r_stat.zsh
@@ -1,9 +1,11 @@
 if [[ -x `which R` ]]; then
 	export R_HOME=`Rscript -e 'cat(Sys.getenv()[["R_HOME"]])'`
 	if [[ $OSTYPE == "linux-gnu" ]]; then
-		alias Rinstall="R CMD INSTALL --library=$R_HOME/site-library"
+		export R_LIBS_SITE="$R_HOME/site-library"
 	else
-		alias Rinstall="R CMD INSTALL --library=$R_HOME/library"
+		export R_LIBS_SITE="$R_HOME/library"
 		manpath=($R_HOME $manpath)
 	fi
+	alias Rinstall="sudo R CMD INSTALL --library=$R_LIBS_SITE"
+	alias Runisntall="sudo R CMD REMOVE  --library=$R_LIBS_SITE"
 fi
