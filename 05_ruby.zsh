994c87c9ffd73fe3418bc48459bb8e6dfeeaafff
James Harris <jimmy@jamesharris.org>
2012-02-17 21:48:58 -0600

    system independent rvm

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -3,3 +3,7 @@ alias irb='irb --readline -r irb/completion'
 if [[ -s /Users/ramiel/.rvm/scripts/rvm ]] ; then 
   source /Users/ramiel/.rvm/scripts/rvm ; 
 fi
+
+if [[ -s /usr/local/rvm/scripts/rvm ]] ; then 
+  source /usr/local/rvm/scripts/rvm ; 
+fi
