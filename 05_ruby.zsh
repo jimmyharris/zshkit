899c150f3688da35ded668693b71a66d40b5d879
James Harris <jimmy@jamesharris.org>
2012-02-20 13:13:40 -0600

    Fixed rbenv support

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -2,19 +2,22 @@ alias irb='irb --readline -r irb/completion'
 
 # Rbenv support
 #
-if [[ -d $HOME/.rbenv ]] ; then 
-  RBENV_DIR= "$HOME/.rbenv"
-elif [[ -d /usr/local/rbenv ]] ; then 
-  RBENV_DIR= "/usr/local/rbenv" 
+# Rbenv support
+#
+if [[ -d "${HOME}/.rbenv/bin" ]]; then
+  rbenv_root="${HOME}/.rbenv"
+elif [[ -d "/usr/local/rbenv/bin" ]]; then
+  rbenv_root="/usr/local/rbenv"
+  export RBENV_ROOT="$rbenv_rooth"
 elif [[ -s /Users/ramiel/.rvm/scripts/rvm ]] ; then 
   source /Users/ramiel/.rvm/scripts/rvm ; 
 elif [[ -s /usr/local/rvm/scripts/rvm ]] ; then 
   source /usr/local/rvm/scripts/rvm ; 
 fi
 
-if [[ -n "$RBENV_DIR" ]]; then
-  path = ("$RBENV_DIR" $path)
-  eval `rbenv init`
+if [[ -n $rbenv_root ]]; then
+  path=($rbenv_root/bin $path)
+  eval "$(rbenv init -)"
 fi
 
 typeset -U fpath path manpath
