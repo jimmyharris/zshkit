94099b7283c7912d687b8aab73246dfb51d2feee
James Harris <jimmy@jamesharris.org>
2012-02-20 12:35:08 -0600

    Added rbenv support. Prefers rbenv to rvm.

--- 05_ruby.zsh
+++ 05_ruby.zsh
@@ -1,9 +1,20 @@
 alias irb='irb --readline -r irb/completion'
 
-if [[ -s /Users/ramiel/.rvm/scripts/rvm ]] ; then 
+# Rbenv support
+#
+if [[ -d $HOME/.rbenv ]] ; then 
+  RBENV_DIR= "$HOME/.rbenv"
+elif [[ -d /usr/local/rbenv ]] ; then 
+  RBENV_DIR= "/usr/local/rbenv" 
+elif [[ -s /Users/ramiel/.rvm/scripts/rvm ]] ; then 
   source /Users/ramiel/.rvm/scripts/rvm ; 
-fi
-
-if [[ -s /usr/local/rvm/scripts/rvm ]] ; then 
+elif [[ -s /usr/local/rvm/scripts/rvm ]] ; then 
   source /usr/local/rvm/scripts/rvm ; 
 fi
+
+if [[ -n "$RBENV_DIR" ]]; then
+  path = ("$RBENV_DIR" $path)
+  eval `rbenv init`
+fi
+
+typeset -U fpath path manpath
