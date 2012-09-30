215359fe479ccf0ebd61431fc221dc4f0b1202a3
James Harris <jimmy@jamesharris.org>
2012-09-29 23:14:42 -0500

    Cleaned up completion configuration.
    Removed some redundant completion includes and logic (like pip
    completion). This is now handled in the "completions" submodule.

--- 07_completion.zsh
+++ 07_completion.zsh
@@ -55,36 +55,9 @@ zstyle ':completion:*' list-colors "$LS_COLORS"
 
 if [[ -o login ]]; then
 
-  if [[ ! -h $ZSHKIT/func/_rvm ]]; then
-    if [[ -f /usr/local/rvm/scripts/zsh/Completion/_rvm ]]; then
-      ln -s "/usr/local/rvm/scripts/zsh/Completion/_rvm" $ZSHKIT/func/_rvm
-    fi
-    if [[ -f $HOME/.rvm/scripts/zsh/Completion/_rvm ]]; then
-      ln -s "$HOME/.rvm/scripts/zsh/Completion/_rvm" $ZSHKIT/func/_rvm
-    fi
-  fi
   if [[ -f "/usr/local/Library/Contributions/brew_zsh_completion.zsh" ]]; then
     if [[ ! -h $ZSHKIT/func/_brew ]]; then
       ln -s "/usr/local/Library/Contributions/brew_zsh_completion.zsh" $ZSHKIT/func/_brew
-    fi
-  fi
-
-  if [[ -e `which pip` ]]; then
-    if [[ ! -d "$ZSHKIT/zsh-pip-completion" ]]; then
-      echo 'For better PyPI completion in zsh please run:'
-      echo ''
-      if [[ -e `which github` ]]; then
-        echo '	cd $ZSHKIT'
-        echo '	github clone jimmyharris zsh-pip-completion'
-      else
-        echo '	cd $ZSHKIT'
-        echo '	git clone git://github.com/jimmyharris/zsh-pip-completion.git'
-        echo ''
-      fi
-      eval `pip completion --zsh`
-    else
-      fpath=($ZSHKIT/zsh-pip-completion $fpath)
-      typeset -U fpath
     fi
   fi
 
