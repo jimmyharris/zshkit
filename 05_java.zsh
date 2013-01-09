b2e9e7d9de361530b814ff5424ab7d47d1140664
James Harris <jimmy@jamesharris.org>
2013-01-09 14:00:30 -0600

    Fix Path and environment vars for java and node.
    Make sure npm path is setup correctly on OS X. Also make sure that
    JAVA_HOME on OS X.

--- 05_java.zsh
+++ 05_java.zsh
@@ -1,4 +1,7 @@
 if [[ $OSTYPE == "linux-gnu" ]]; then
   JAVA_HOME="/usr/java/default"
   export JAVA_HOME
+elif [[ $OSTYPE[1,6] == "darwin" ]]; then
+  JAVA_HOME=`/usr/libexec/java_home`
+  export JAVA_HOME
 fi
--- 05_node.zsh
+++ 05_node.zsh
@@ -6,6 +6,6 @@ fi
 
 if [[ (-d "/usr/local/lib/node") ]]; then
   NODE_PATH="/usr/local/lib/node"
-  path=(/usr/local/share/node/npm/bin $path)
+  path=(/usr/local/share/npm/bin $path)
   export NODE_PATH
 fi
