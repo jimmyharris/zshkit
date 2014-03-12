81ee5cef5de9756323c3b378112565f95487c730
James Harris <james.harris@garmin.com>
2014-03-12 13:45:12 -0500

    Whitespace Fixes.
    Removing trailing whitespace from files as I find it.

--- 05_java.zsh
+++ 05_java.zsh
@@ -1,5 +1,5 @@
 if [[ $OSTYPE == "linux-gnu" ]]; then
-  JAVA_HOME="/usr/java/default"
+  JAVA_HOME="/usr/lib/jvm/java"
   export JAVA_HOME
 elif [[ $OSTYPE[1,6] == "darwin" ]]; then
   JAVA_HOME=`/usr/libexec/java_home`
