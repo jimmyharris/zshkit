8db77f53eeeb6ef2135a2b977fe0e76201b3fbc4
Jimmy Harris <james.russell.harris@gmail.com>
2015-10-07 10:13:14 -0500

    Add cabal path for pandoc support
    Since I need to install lua from source on Ubuntu I have to build pandoc
    from source rather than install the Ubuntu built lua 5.1 library.

---
+++ 05_haskell.zsh
@@ -0,0 +1,9 @@
+
+# If we have installed cabal locally then use it to generate pandoc documents.
+if [[ -d "$HOME/.cabal/bin" ]]; then
+  path=(~/.cabal/bin $path)
+fi
+
+# cleanup the path.
+typeset -U path
+
