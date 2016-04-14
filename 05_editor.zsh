6996627b4b26d30f6b7f419bed2c5d7d844df159
Jimmy Harris <james.russell.harris@gmail.com>
2016-04-14 11:12:24 -0500

    Drop suffix aliases.
    Suffix aliases make it so that I can't run scripts that have a suffix
    they recognize. This makes them more of a hinderance than a useful aid
    to daily work. I am dropping them for now. Also TextMate is dead, I am
    always going to use either vim or nvim.

--- 05_editor.zsh
+++ 05_editor.zsh
@@ -9,8 +9,6 @@ if [[ -x `which nvim` ]]; then
 	EDITOR=nvim
 elif [[ -x `which vim` ]]; then
 	EDITOR=vim
-elif [[ -x `which mate` && $(not_run_from_ssh) = 1 ]]; then
-	EDITOR="mate -w"
 elif [[ -x `which nano` ]]; then
 	EDITOR=nano
 elif [[ -x `which pico` ]]; then
@@ -20,19 +18,4 @@ else
 fi
 
 export EDITOR
-
-# Set EDITOR as default for plaintext stuff
-for s in txt c cc cxx cpp lua py; do
-	alias -s $s=$EDITOR
-done
-
-# Abuse the "open" command on OS X
-if [[ $OSTYPE[1,6] == "darwin" ]]; then
-	for s in mp3 wav aac \
-		avi mp4 m4v mov qt mpg mpeg \
-		jpg png psd bmp gif tif tiff \
-		ps pdf html dmg; do
-		alias -s $s=open
-	done
-fi
 
