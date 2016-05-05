a18c96d45558b3bb40d884f3dbc6ec903a204bb6
James Harris <james.harris@garmin.com>
2016-05-05 11:07:13 -0500

    Minor miscelaneous clean up
    clean up some things to make this work better with my work ubuntu
    machine.

--- 06_git.zsh
+++ 06_git.zsh
@@ -1,28 +1,5 @@
 if [[ -x `which git` ]]; then
 	alias g=git
-	function git-branch-name () {
-		git branch 2> /dev/null | grep '^\*' | sed 's/^\*\ //'
-	}
-	function git-dirty () {
-		git status 2> /dev/null | grep "nothing to commit (working directory clean)"
-		echo $?
-	}
-	function gsrb () {
-		branch=$(git-branch-name)
-		git checkout master
-		git svn rebase
-		git checkout "${branch}"
-		git rebase master
-	}
-	function git-scoreboard () {
-		git log | grep '^Author' | sort | uniq -ci | sort -r
-	}
-	function git-track () {
-		branch=$(git-branch-name)
-		git config branch.$branch.remote origin
-		git config branch.$branch.merge refs/heads/$branch
-		echo "tracking origin/$tracking"
-	}
 	if [[ -s `which hub` ]] ; then
     alias g='hub'
 	fi
