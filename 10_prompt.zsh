eac4bd1f264d8cf77adc76dfd7bf22360c437e9e
Jimmy Harris <james.russell.harris@gmail.com>
2016-05-05 11:07:13 -0500

    Minor miscelaneous clean up
    clean up some things to make this work better with my work ubuntu
    machine.

--- 10_prompt.zsh
+++ 10_prompt.zsh
@@ -1,10 +1,11 @@
 if [[ -o interactive ]]; then
-  export PROMPT=""
-  fpath=($ZSHKIT/zsh-git/functions $fpath)
-  typeset -U fpath
+  export PROMPT="%# "
+  export RPROMPT=""
+  # fpath=($ZSHKIT/zsh-git/functions $fpath)
+  # typeset -U fpath
   setopt prompt_subst
 	autoload -U colors
 	autoload -U promptinit
   promptinit
-  prompt wunjo
+  # prompt wunjo
 fi
