ca8a18acccc9395127878519d17ed03edf8d0f86
Jimmy Harris <james.russell.harris@gmail.com>
2018-09-24 09:38:57 -0500

    Fix Java path lookups
    This is a java path lookup that needs to be fixed. This assumes some
    very specific things from work.

--- rc/05_java.zsh
+++ rc/05_java.zsh
@@ -1,14 +1,20 @@
+#TODO: make this a lookup path that is configurable via local configuration.
+# Add additional support for openJDK fallback cases.
 if [[ $OSTYPE == "linux-gnu" ]]; then
-  if [[ -d "/usr/lib/jvm/java-7-oracle" ]]; then
-    JAVA_HOME="/usr/lib/jvm/java-7-oracle"
-  elif [[ -d "/usr/lib/jvm/java-1.7.0-openjdk-amd64" ]]; then
-    JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk-amd64"
+  if [[ -d "/opt/java/jdk1.8.0_152" ]]; then
+    JAVA_HOME="/opt/java/jdk1.8.0_152"
+  elif [[ -d "/usr/lib/jvm/java-8-oracle" ]]; then
+    JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+  elif [[ -d "/usr/lib/jvm/java-1.8.0-openjdk-amd64" ]]; then
+    JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
   elif [[ -d "/usr/lib/jvm/java" ]]; then
     JAVA_HOME="/usr/lib/jvm/java"
   else
     JAVA_HOME="/usr/lib/jvm/jre"
   fi
   export JAVA_HOME
+  # Make sure javahome is first in our path before system java.
+  path=($JAVA_HOME/bin $path)
 elif [[ $OSTYPE[1,6] == "darwin" ]]; then
   JAVA_HOME=`/usr/libexec/java_home`
   export JAVA_HOME
